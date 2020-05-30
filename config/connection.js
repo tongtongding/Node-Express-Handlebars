cb67c1536f0051c6393fdc0030cb07810de60fad
tongtong ding <tding7@asu.edu>
2020-05-30 14:22:32 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql");
 const util = require("util");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mushroom",
-    database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "mushroom",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function (error) {
     if (error) {
